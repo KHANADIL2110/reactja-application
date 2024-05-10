1fd142ca551096967edd2a654846e33041775a88
khan <er.adilkhan21@gmail.com>
2024-05-10 16:23:03 +0530

    sourse code

---
+++ src/searchbox.js
@@ -0,0 +1,18 @@
+
+import "./searchbox.css"
+
+function Searchbox()
+{
+    return<div className="Box">
+        <div id="loc">
+            
+        </div>
+        
+        
+        
+
+      
+
+    </div>
+}
+export default Searchbox;
